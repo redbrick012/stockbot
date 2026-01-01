1e8ae1fd661129d853529a81e1651d816ed309e5
redbrick012 <157280932+redbrick012@users.noreply.github.com>
2026-01-01 17:03:51 +0000

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,10 +1,16 @@
+import os
+import json
 import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
-creds = Credentials.from_service_account_file(
-    "credentials.json",
+service_account_info = json.loads(
+    os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]
+)
+
+creds = Credentials.from_service_account_info(
+    service_account_info,
     scopes=SCOPES
 )
 
@@ -13,4 +19,3 @@ client = gspread.authorize(creds)
 def get_sheet_values(sheet_name):
     sheet = client.open(sheet_name).sheet1
     return sheet.get_all_values()
-
